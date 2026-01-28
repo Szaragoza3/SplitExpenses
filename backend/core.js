a5d90fee861c5e202818d2dc6afd668f6ffdc8ed
Sergio Zaragoza Navarro <sergiozaragoza19@gmail.com>
2026-01-28 17:39:52 +0100

    Avance front + función add_expense

--- backend/core.js
+++ backend/core.js
@@ -8,3 +8,31 @@ function add_participants(names, participants) {
         }
     }
 }
+
+function add_expense(paid_by, amount, involved, concept, expenses, participants) {
+    if (!participants.includes(paid_by)) {
+        console.log(paid_by + " no está en el grupo.");
+        return;
+    }
+    if (amount < 0) {
+        console.log("Error: El gasto " + concept + " de " + amount + "€ no se añadió por valor negativo");
+        return;
+    }
+    for (let person of involved) {
+        if (!participants.includes(person)) {
+            console.log(person + " no está en el grupo.");
+            return;
+        }
+    }
+    let expense = {
+        paid_by: paid_by,
+        amount: amount,
+        involved: involved,
+        concept: concept
+    }
+    expenses.push(expense);
+
+    console.log(
+        "El gasto " + concept + " de" + amount + "€ se añadió, fue pagado por " + paid_by + ", para" + involved.join(", ")
+    );
+}
