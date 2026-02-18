942a8d56f37a3722f27195f0213767217d71a420
Sergio Zaragoza Navarro <sergiozaragoza19@gmail.com>
2026-02-18 06:12:07 +0100

    avance 18/02

--- frontend/dist/app.js
+++ frontend/dist/app.js
@@ -1,6 +1,6 @@
 const state = {
-    groupName: "",
-    participants: []
+  groupName: "",
+  participants: []
 };
 
 const groupNameInput = document.getElementById("group-name-input");
@@ -8,20 +8,27 @@ const groupNameInput = document.getElementById("group-name-input");
 const participantsContainer = document.getElementById("participants-container");
 const addParticipantsBtn = document.getElementById("add-participants-btn");
 
-const form = document.getElementById("group-form");
+const groupForm = document.getElementById("group-form");
 
-addParticipantsBtn.addEventListener("click", addParticipant)
+if (addParticipantsBtn) {
+  addParticipantsBtn.addEventListener("click", addParticipant);
+  addParticipantsBtn.addEventListener("click", updateFooterPosition);
+}
 
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
+if (groupForm) {
+  groupForm.addEventListener("submit", (e) => {
+    e.preventDefault();
 
-  console.log("Grupo:", state.groupName);
-  console.log("Participantes:", state.participants);
-});
+    console.log("Grupo:", state.groupName);
+    console.log("Participantes:", state.participants);
+  });
+}
 
-groupNameInput.addEventListener("input", (e) => {
-  state.groupName = e.target.value;
-});
+if (groupNameInput) {
+  groupNameInput.addEventListener("input", (e) => {
+    state.groupName = e.target.value;
+  });
+}
 
 const firstParticipantInput = document.querySelector(".participant-input");
 if (firstParticipantInput) {
@@ -36,7 +43,7 @@ function addParticipant() {
     input.type = "text";
     input.id = `participant-${index}`;
     input.classList.add("participant-input");
-    input.placeholder = "Nombre del participante";
+    input.placeholder = `Nombre del participante nº${index+1}`;
     input.dataset.index = index;
 
     participantsContainer.appendChild(input);
@@ -50,4 +57,35 @@ function attachParticipantListener(input) {
     const index = Number(e.target.dataset.index);
     state.participants[index] = e.target.value;
   });
+}
+
+const footer = document.getElementById("footer");
+
+function updateFooterPosition() {
+  const checkScroll =
+    document.documentElement.scrollHeight > window.innerHeight;
+
+  if (checkScroll) {
+    footer.classList.remove("card-footer");
+    footer.classList.add("card-footer-static");
+  } else {
+    footer.classList.add("card-footer");
+    footer.classList.remove("card-footer-static");
+  }
+}
+
+window.addEventListener("load", updateFooterPosition);
+window.addEventListener("resize", updateFooterPosition);
+
+const printGroupNamebtn = document.getElementById("print-groupList-btn");
+
+if (printGroupNamebtn) {
+  printGroupNamebtn.addEventListener("click", printGroupName)
+}
+
+function printGroupName () {
+  const currentDiv = document.getElementById("group-list");
+  if (!currentDiv) return;
+
+  currentDiv.textContent = state.groupName.value;
 }
